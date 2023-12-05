8eb0f222582bb8d9c83bca62cf10e03a188047a5
Georg Hertkorn <ghertko@gwdg.de>
2023-12-05 15:43:30 +0100

    update from FactGrid: initial tests

---
+++ src/Controller/FactGridController.php
@@ -0,0 +1,140 @@
+<?php
+namespace App\Controller;
+
+use App\Service\FactGridService;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class FactGridController extends AbstractController {
+    // route '/edit' is globally protected (see security.yaml)
+
+
+    /**
+     * update FactGridIds
+     *
+     * display page with update options
+     *
+     * @Route("/edit/factgrid/update", name="edit_factgrid_update")
+     */
+    public function update(Request $request,
+                           EntityManagerInterface $entityManager,
+                           FactGridService $service) {
+
+        $service->queryWIAGItems();
+
+        return new Response("FactGridController->update()");
+
+        // return $this->renderForm('monastery/update.html.twig', [
+        //     'menuItem' => 'edit-menu',
+        // ]);
+    }
+
+    /**
+     * update monastery data
+     *
+     * @Route("/edit/monastery/update-step", name="edit_monastery_update_step")
+     */
+    function updateStep(Request $request,
+                        MonasteryService $service,
+                        EntityManagerInterface $entityManager) {
+
+        $debug_flag = false;
+        $chunk_size = $request->query->get('chunkSize');
+        $offset = $request->query->get('offset');
+
+        $list = [];
+        $set_size = 0;
+        $count = 0;
+        $status = 200;
+        try {
+            $set_size_n_list = $service->queryGSList($chunk_size, $offset);
+            $list = $set_size_n_list["list"];
+            $set_size = $set_size_n_list["set_size"];
+        } catch (Exception $e) {
+            $status = $e->getMessage();
+        }
+
+        // dd($status, $list);
+        $updated_n = 0;
+        $created_n = 0;
+
+        if ($status == 200) {
+
+            foreach ($list as $gsn) {
+                $repository = $entityManager->getRepository(Institution::class);
+                $monastery_q_list = $repository->findByIdGsn($gsn);
+
+                if (!is_null($monastery_q_list) && count($monastery_q_list) > 0) {
+                    $monastery = array_values($monastery_q_list)[0];
+                    $service->update($monastery);
+                    $updated_n += 1;
+                } else {
+                    $user_id = intval($this->getUser()->getId());
+                    $monastery_new = new Institution($user_id);
+                    $monastery_new->setIdGsn($gsn);
+                    // $monastery_new->setCorpusId(Institution::CORPUS_ID); 2023-11-16
+                    $service->update($monastery_new);
+                    $entityManager->persist($monastery_new->getItem());
+                    $entityManager->persist($monastery_new);
+
+                    $item_corpus = new ItemCorpus();
+                    $item_corpus->setItem($monastery_new->getItem());
+                    $item_corpus->setCorpusId(Institution::CORPUS_ID);
+                    $item_corpus->setIdInCorpus($gsn);
+                    $entityManager->persist($item_corpus);
+
+                    $created_n += 1;
+                }
+            }
+            $entityManager->flush();
+
+            $count = count($list);
+            // done ?
+            if ($count < $chunk_size) {
+                $status = 240;
+            }
+
+            //debug
+            // $max_offset = 500;
+            // if ($offset > $max_offset) {
+            //     $status = 240;
+            // }
+        }
+
+
+        if ($debug_flag) {
+            // access dump data
+            $template = 'monastery/debug_status.html.twig';
+        } else {
+            $template = 'monastery/_status.html.twig';
+        }
+
+        $response = $this->render($template, [
+            'menuItem' => 'edit-menu',
+            'status' => $status,
+            'count' => $count,
+            'totalCount' => $offset + $updated_n + $created_n,
+            'maxCount' => $set_size,
+            'createdCount' => $created_n,
+        ]);
+
+        $response->setStatusCode($status);
+
+        return $response;
+    }
+
+
+}
---
+++ src/Service/FactGridService.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Service;
+
+
+use App\Entity\Item;
+use App\Entity\Institution;
+use App\Entity\InstitutionPlace;
+use App\Entity\Place;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class FactGridService {
+
+    const URL_SPARQL = "https://database.factgrid.de/sparql";
+
+    private $client;
+    private $entityManager;
+
+    public function __construct(HttpClientInterface $client,
+                                EntityManagerInterface $entityManager) {
+        $this->client = $client;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * queryWIAGItems()
+     *
+     * TODO
+     */
+        public function queryWIAGItems() {
+
+        $sparql_query = "SELECT DISTINCT ?item ?itemLabel ?wiag ".
+                      "WHERE { ?item wdt:P601 ?wiag. ?item wdt:P2 wd:Q7. ".
+                      "SERVICE wikibase:label { bd:serviceParam wikibase:language ".
+                      "\"[AUTO_LANGUAGE],en\". }}";
+
+        $response = $this->client->request('GET', self::URL_SPARQL, [
+            'query' => [
+                'query' => $sparql_query,
+            ]
+        ]);
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        if ($statusCode != 200) {
+            throw new Exception($statusCode);
+        }
+        $contentType = $response->getHeaders()['content-type'][0];
+        dump($contentType);
+        $content = $response->getContent();
+        $xml_object = simplexml_load_string($content);
+        $n = 0;
+        foreach ($xml_object->children() as $xml_child) {
+            dump($xml_child);
+            $n += 1;
+            if ($n >= 22) {
+                break;
+            }
+        }
+        dd("ende");
+
+    }
+
+    /**
+     * queryGSList($chunk_size, $offset)
+     *
+     * @return status code and list of gsn
+     */
+    public function queryGSList($chunk_size, $offset) {
+
+        $response = $this->client->request('GET', self::LIST_URL_GS, [
+            'query' => [
+                'limit' => $chunk_size,
+                'offset' => $offset
+            ]
+        ]);
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        if ($statusCode != 200) {
+            throw new Exception($statusCode);
+        }
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        // $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        $set_size = $content["total"];
+
+        // extract gsn
+        $gsn_list = [];
+        foreach($content["list"] as $monastery) {
+            $gsn_list[] = $monastery["gsnId"];
+        }
+
+        return [
+            "set_size" => $set_size,
+            "list" => $gsn_list,
+        ];
+
+    }
+
+    /**
+     * update($monastery)
+     *
+     * update institution where institution.idGsn is $gsn; fetch $data via call to the Klosterdatenbank API
+     * corpus_id is not set nor overwritten here
+     */
+    public function update($monastery) {
+
+        $gsn = $monastery->getIdGsn();
+        $data = $this->queryGSByGsn($gsn);
+
+        // via the API we cannot access gs_monastery.id_monastery
+        $monastery->setName($data["name"]);
+
+        if (array_key_exists("note", $data)) {
+            $field_size = 2047;
+            $monastery->setNote(substr($data["note"], 0, $field_size));
+        }
+
+        // update $monastery->institutionPlace
+        // - remove entries
+        $ip_list = $monastery->getInstitutionPlace();
+        foreach ($ip_list as $ip) {
+            $ip_list->removeElement($ip);
+            $ip->setInstitution(null);
+            $this->entityManager->remove($ip);
+        }
+
+        $placeRepository = $this->entityManager->getRepository(Place::class);
+        foreach ($data["locations"] as $data_location) {
+            $ip_new = new InstitutionPlace();
+            $ip_new->setInstitution($monastery);
+            $ip_list->add($ip_new);
+            $place = $placeRepository->findByGeonamesId($data_location["geonamesId"]);
+
+            // set place
+            // - examine missing places in an extra process
+            if (!is_null($place) && count($place) > 0) {
+                $ip_new->setPlaceId($place[0]->getId());
+                $ip_new->setPlaceName($place[0]->getName());
+            } else {
+                $ip_new->setPlaceName($data_location["placeName"]);
+            }
+
+            // set dates
+            $this->setDates($ip_new, $data_location);
+
+            $this->entityManager->persist($ip_new);
+        }
+
+        return $monastery;
+    }
+
+    private function setDates($institution_place, $data) {
+        // set dates
+        // 2022-11-14 TAQ-values are not accessible via the API
+        $institution_place->setNoteBegin($data["from"]);
+        $institution_place->setNoteEnd($data["to"]);
+        $date_key_fnc_list = [
+            "beginTPQ" => "setDateBeginTpq",
+            "endTPQ" => "setDateEndTpq",
+            "beginTAQ" => "setDateBeginTaq",
+            "endTAQ" => "setDateEndTaq"
+        ];
+        $this->setByKeyList($institution_place, $data, $date_key_fnc_list);
+
+        // set num_date_begin, num_date_end
+        $num_date_begin = $institution_place->getDateBeginTpq();
+        if (!is_null($num_date_begin)) {
+            $institution_place->setNumDateBegin($num_date_begin);
+        } else {
+            $num_date_begin = $institution_place->getDateBeginTaq();
+            if (!is_null($num_date_begin)) {
+                $institution_place->setNumDateBegin($num_date_begin - self::DATE_MARGIN);
+            }
+        }
+
+        $num_date_end = $institution_place->getDateEndTaq();
+        if (!is_null($num_date_end)) {
+            $institution_place->setNumDateEnd($num_date_end);
+        } else {
+            $num_date_end = $institution_place->getDateEndTpq();
+            if (!is_null($num_date_end)) {
+                $institution_place->setNumDateEnd($num_date_end + self::DATE_MARGIN);
+            }
+        }
+        return $institution_place;
+    }
+
+    /**
+     *
+     */
+    private function setByKeyList($obj, $data, $key_fnc_list) {
+        foreach($key_fnc_list as $key => $fnc) {
+            if (array_key_exists($key, $data)) {
+                $obj->$fnc($data[$key]);
+            }
+        }
+    }
+
+    /**
+     * queryGSByGsn($gsn)
+     *
+     * @return status code and list of gsn
+     */
+    private function queryGSByGsn($gsn) {
+
+        $response = $this->client->request('GET', self::SINGLE_URL_GS.'/'.$gsn);
+
+        $statusCode = $response->getStatusCode();
+        if ($statusCode != 200) {
+            throw new Exception($statusCode);
+        }
+
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $content = $response->getContent(); // json
+        $content = $response->toArray();
+
+        return $content;
+    }
+
+};
