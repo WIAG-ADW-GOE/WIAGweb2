a921b7c4dbc94e9ead6ce774b0e578baca45f131
Felix Schwart <util.fs@tuta.com>
2025-03-12 12:59:13 +0100

    sync_notes: combining the import steps and in general cleaning up further

--- notebooks/sync_notebooks/scripts/pr_recon.py
+++ notebooks/sync_notebooks/scripts/pr_recon.py
@@ -4,132 +4,81 @@
 # Updates DPr with the WIAG IDs assigned to each person, ensuring DPr remains the primary and up-to-date source of data.
 
 # ## Requirements
-# * You need access to the following sql databases at https://vwebfile.gwdg.de/phpmyadmin/:
-#   * wiag database
-#   * Digitales Personenregister database
+# You need access to the following sql databases at https://vwebfile.gwdg.de/phpmyadmin/:
+# * wiag database
+# * Digitales Personenregister database
 # 
 # In you can't access the database at the link above, please check if you are in the GWDG network. You can fix this by using the GWDG VPN.
 #
 # ## Steps in the notebook
-# 1. Get data from WIAG
-# 
-# 2. Get data from the Digitales Personenregister (DPr)
+# 1. Export data from WIAG and DPr
+#
+# 2. Import the files
 # 
 # 3. Check for problematic entries
 # 
-# 4. If everything is fine, check for outdated entries
+# 4. Check for outdated entries
 # 
 # 5. Generate SQL file to update the outdated entries in the Digitales Personenregister
 
 # %% [markdown]
-# ## Get data from wiag
+# ## 1. Export data from WIAG and DPr
 #
-# ### Run the SQL query
+# For this step you have to manually export the datasets by opening [phpMyAdmin](https://vwebfile.gwdg.de/phpmyadmin/) and then for WIAG and DPr each:
+# 1. log in 
+# 2. run the saved "Step 2 of the sync notebooks for WIAG" sql query
+# 3. export the result to a csv file
+# 
+# A detailed description can be found here: [Run_SQL_Query_and_Export_CSV.md](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/docs/Run_SQL_Query_and_Export_CSV.md) (As a backup, here are the saved queries: [WIAG](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/scripts/get_wiag_data.sql) -- [DPr](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/scripts/get_dpr_data.sql))
 #
-# Run the "Step 2 of the sync notebooks for WIAG" sql query on the WIAG database (https://vwebfile.gwdg.de/phpmyadmin/) and then export the results as csv. Please refer to the instructions at [Run_SQL_Query_and_Export_CSV.md](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/docs/Run_SQL_Query_and_Export_CSV.md) for a detailed explanation of running the sql command.
 #
-# ### Import the file
-# Please move the downloaded file to the `input_path` directory defined below or change the `input_path` to where the file is located.
+# ## 2. Import the files
+# Please move the downloaded files to the `input_path` directory defined below or change the `input_path` to where the files are located.
 
 # %%
-# first loading necessary libraries
-import requests
-import csv
+import csv # first loading necessary libraries
 import os
 import pandas as pd
-import json
-import re
 import os
-from datetime import datetime
 
 # change this to where the csv file is located (e.g. C:\Users\<your_username_here>\Downloads\) or move the csv file to this directory
 input_path = r"C:\Users\Public\WIAGweb2\notebooks\sync_notebooks\input_files"
 
-filename = 'item.csv' # change this in case you renamed the file
+wiag_file = 'item.csv' # change this in case you renamed the file
+dpr_file = 'persons.csv' # change this in case you renamed the file
 
-ic_df = pd.read_csv(os.path.join(input_path, filename), names=["id", "wiag_id", "gsn"]) # importing the data
+ic_df = pd.read_csv(os.path.join(input_path, wiag_file), names=["id", "wiag_id", "gsn"])
+dpr_df = pd.read_csv(os.path.join(input_path, dpr_file), names=["wiag_id", "id", "gsn_table_id", "gsn"])
 
+# %% [markdown]
+# ## 3. Check for problematic entries
+# Any listed entries **need to be fixed manually** before once again exporting the updated data from WIAG and DPr
+# 
+# First a list of known problematic entries (by GSN) in the DPr is created. These entries are linked to more than one entry in WIAG and it is unclear whether the different entries reference the same person or not, so they should simply be ignored for the rest of the script.
+# %%
+gsns_of_known_problematic_wiag_entries = ['046-02872-001', '007-00413-001']
 # %% [markdown]
 # ### Check data from WIAG
-
-# The following code block should be empty. If this is not empty, you cannot proceed. This problem needs to fixed manually. Please contact Barbara Kroeger to resolve this issue.
-
 # %%
 ic_df[ic_df['gsn'].isna()] # checking for entries with an empty Germania Sacra Number field
-
 # %% [markdown]
-# ## Get data from DPr
-# 
-# ### Run the SQL query
-# This time run the query (with the same name) on the Digitales Personenregister database.
-#
-# ### Import the file
-# Once again, please move the downloaded file to the `input_path` directory defined above.
-
+# checking for entries that reference the same GSN
 # %%
-filename = 'persons.csv' # change this in case you renamed the file
-
-dpr_df = pd.read_csv(os.path.join(input_path, filename), names=["wiag_id", "id", "gsn_table_id", "gsn"])
-
+_duplicates = ic_df[ic_df.duplicated(subset = ['gsn'], keep = False)]
+_duplicates[~_duplicates['gsn'].isin(gsns_of_known_problematic_wiag_entries)].sort_values(by=['gsn']) # ignoring known entries
 # %% [markdown]
 # ### Check data from DPr
-#
-# If both code cell outputs are empty, there are no problems and you can proceed.
-
 # %%
-dpr_df[dpr_df['gsn'].isna()] # checking for entries without a GSN
-
+dpr_df[dpr_df['gsn'].isna()] # checking for entries with an empty Germania Sacra Number field
 # %%
-dpr_df_gp = dpr_df.groupby('wiag_id').count()
-dpr_df_gp[dpr_df_gp['gsn'] > 1] # checking for entries with the same WIAG-ID
+ic_df[ic_df.duplicated(subset = ['wiag_id'], keep = False)].sort_values(by = ['wiag_id']) # checking for entries with the same WIAG-ID
 
 # %% [markdown]
-# ## Check combined data
+# ## 4. Check for entries with (probably) outdated WIAG-IDs in DPr
 #
-# ### Check for multiple records in WIAG for one Germania Sacra Number (GS Number) - manual fix
-# 
-# The code cell below finds GSNs that more than one entry in WIAG are referencing and lists the problematic entries. Should any entries be listed, these need to be fixed manually before proceeding.
-# 
-# In case a GSN should be ignored, e.g. because it is unclear whether the linked WIAG entries reference the same person, add the GSN to the list at the start of the cell and it will be omitted.
-#
-# %%
-# A list of GSNs of known DPr entries which are linked to more than one entry in WIAG, for which it is unclear whether the different entries reference the same person or not.
-gsns_of_known_problematic_wiag_entries = ['046-02872-001', '007-00413-001']
-
-# find duplicates
-gp_df = ic_df.groupby('gsn').count()
-duplicate_wiag_gsns = gp_df[gp_df['id'] > 1].index.to_list()
-
-# remove known entries
-for gsn in gsns_of_known_problematic_wiag_entries:
-    if gsn in duplicate_wiag_gsns: duplicate_wiag_gsns.remove(gsn)
-    else :
-        print("Entry " + gsn + " seems to have been resolved and should most likely be removed from the list. If you are unsure, ask Barbara Kroeger.\n")
-
-# find/merge all relevant information for duplicate entries
-dupl_ppl = ic_df[ic_df['gsn'].isin(duplicate_wiag_gsns)]
-dupl_ppl = dupl_ppl.merge(dpr_df, on='gsn', suffixes=('_wiag', '_dpr'))
-
-# detect special cases where the WIAG-ID of the DPr entry is outdated
-print("Special cases with outdated WIAG-ID in DPr, if any are detected:")
-for gsn in duplicate_wiag_gsns:
-    dpr_wiag_id = dpr_df[dpr_df['gsn'] == gsn]['wiag_id'].values[0]
-    dpr_wiag_id = ["", "1"]
-    if len(dupl_ppl[dupl_ppl['wiag_id_wiag'] == dpr_wiag_id]) == 0:
-        print(dupl_ppl[dupl_ppl['gsn'] == gsn])
-
-# print detected problematic entries (without known entries)
-print('\nAll "normal" cases:')
-dupl_ppl
-
-# %% [markdown]
-# ## Check for entries with (probably) outdated WIAG-IDs in DPr
-#
-# Compares the records downloaded from WIAG and DPr. The output lists entries with differing WIAG-IDs in WIAG and DPr. Check the listed entries to make sure these DPr entries simply need their WIAG-ID updated! Should you be unsure, do not proceed, but contact Barbara Kroeger!
+# Compares the records downloaded from WIAG and DPr. The output lists entries with differing WIAG-IDs in WIAG and DPr. **Check a sample** to make sure the listed entries simply need their WIAG-ID updated and a reasonable amount of entries are listed! For this purpose the list is also saved as a csv-file. Should you be unsure, do not proceed, but contact Barbara Kroeger!
 # 
 # Should the output be empty, there is nothing to be updated and you can proceed with the third notebook. Otherwise, proceed below.
-# 
-# In the step above a list of problematic entries, that should be ignored in both steps, is defined. Currently (2025-02) there are two such DPr entries which each have two WIAG entries linked to them, because it is unclear whether the WIAG entries reference the same person (neither can the entries be merged, nor should a separate DPr entry be created). Should you wish to have an entry ignored for a similar reason, simply add the GSN to the list.
 
 # %%
 # Join the dataframes from WIAG and DPr
@@ -139,42 +88,50 @@ joined_df = ic_df.merge(dpr_df, on='gsn', suffixes=('_wiag', '_dpr'))
 unequal_df = joined_df[joined_df['wiag_id_wiag'] != joined_df['wiag_id_dpr']]
 
 # remove known entries that should be ignored (defined in step "Multiple records in WIAG for one GSN (manual fix)")
-for entry in gsns_of_known_problematic_wiag_entries:
-    index = unequal_df[unequal_df['gsn'] == entry].index
-    if(index.empty):
-        print("Entry " + entry + " seems to have been resolved and should most likely be removed from the list. If you are unsure, ask Barbara Kroeger.")
-    else:
-        unequal_df = unequal_df.drop(index)
+unequal_df = unequal_df[~unequal_df['gsn'].isin(gsns_of_known_problematic_wiag_entries)]
 
-unequal_df
+unequal_df # print entries that will be updated
+# %% [markdown]
+# Saving the list of entries to be updated as a csv-file for easier checking of proposed updates.
+# Change the `output_path` to where you want the csv-file to be output.
+# %%
+from datetime import datetime
+
+output_path = r"C:\Users\Public\WIAGweb2\notebooks\sync_notebooks\output_files"
+today_string = datetime.now().strftime('%Y-%m-%d')
+
+unequal_df.to_csv( 
+    os.path.join(
+        output_path,
+        f'dpr_entries_to_be_updated_{today_string}.csv'
+    ),
+    index=False
+)
 
 # %% [markdown]
-# ## Generate sql file to update DPr
-# 
-# Change the `output_path` to where you want the sql file to be output.
+# ## 5. Updating Digitales Personenregister
+# ### Generating the SQL-file
+# Using the same `output_path` as above.
 #
-# Please run the code cell below. After generating the file, please open the DPr database on phpmyAdmin and run the SQL file there. You can do this by copying the text in the file and pasting it as sql in the interface (same instructions as [Run_SQL_Query_and_Export_CSV.md](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/docs/Run_SQL_Query_and_Export_CSV.md)). The file is located in the `output_files` folder or the `output_path` if you changed it.
-# 
-# Note: 
-#  - Please do a quick check to see if there are a reasonable number of lines in the sql file (for eg: 20-200). If there are thousands of lines, this might indicate that something has gone wrong.
-# 
-#  - Another way to check if the generated SQL file is correct is by checking the GSNs (Germania Sacra Numbers). In the SQL file you will find lines with `-- id: <gsn>`. Please copy the id at the end of the following url: `https://personendatenbank.germania-sacra.de/index/gsn/<gsn>` and check if the wiag id noted there is missing or is incorrect by clicking on the link.
-
 # %%
-output_path = r"C:\Users\Public\WIAGweb2\notebooks\sync_notebooks\output_files"
-
 query = "LOCK TABLES persons WRITE;\n"
-for _, row in unequal_df.iterrows():
+for row in unequal_df.itertuples():
     query += f"""
     UPDATE persons
-    SET wiag = '{row['wiag_id_wiag']}'
-    WHERE id = {row['id_dpr']}; -- id: {row['gsn']}
+    SET wiag = '{row.wiag_id_wiag}'
+    WHERE id = {row.id_dpr}; -- id: {row.gsn}
 """
 query += "\nUNLOCK TABLES;"
+
+today_string = datetime.now().strftime('%Y-%m-%d')
 with open(os.path.join(output_path, f'update_dpr_{today_string}.sql'), 'w') as file:
     file.write(query)
 
 # %% [markdown]
-# You can now continue with the [third notebook here](https://github.com/WIAG-ADW-GOE/WIAGweb2/blob/main/notebooks/sync_notebooks/factgrid_recon.ipynb)
-
-
+# ### Upload the file
+# Once the file has been generated, please open the DPr database on phpmyAdmin and run the SQL file there. First you need to select the database (gsdatenbank) and then either:
+# - go to the Import tab -> select the file -> click 'Ok' to run it
+# - go to the SQL tab -> copy the text in the file and paste it into the interface -> click 'Ok' to run it
+# 
+#
+# Once the update is done, you can continue with the next notebook (fg_and_WIAG-recon - combining former steps 3 and 4)
